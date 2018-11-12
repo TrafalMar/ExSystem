5debff698a71721327bfb78218eace1df241e65c
TrafalMar <vladiksav2@gmail.com>
2018-11-12 23:54:29 +0200

    Worked selector
    *Create basic dinamic of ExSystem
    *Create expanders

--- ExSystem/ExSystem/Form1.Designer.cs
+++ ExSystem/ExSystem/Form1.Designer.cs
@@ -28,29 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Перевірте підключення всі проводів до монітора і розеток");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Назжміть кнопку ВКЛ на моніторі");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Віднесіть монітор на ремонт");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Не працює монітор", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3});
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Не працює клавіатура");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Не працює миша");
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Не завантажується комп\'ютер");
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Немає з\'єднання з інтернет мережею");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Дзвоніть в поліцію");
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Пропав комп\'ютер", new System.Windows.Forms.TreeNode[] {
-            treeNode9});
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("У вас комп\'ютер", new System.Windows.Forms.TreeNode[] {
-            treeNode4,
-            treeNode5,
-            treeNode6,
-            treeNode7,
-            treeNode8,
-            treeNode10});
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("У вас телефон");
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("У вас шизофренія");
+            System.Windows.Forms.TreeNode treeNode27 = new System.Windows.Forms.TreeNode("Перевірте підключення всі проводів до монітора і розеток");
+            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("Назжміть кнопку ВКЛ на моніторі");
+            System.Windows.Forms.TreeNode treeNode29 = new System.Windows.Forms.TreeNode("Віднесіть монітор на ремонт");
+            System.Windows.Forms.TreeNode treeNode30 = new System.Windows.Forms.TreeNode("Не працює монітор", new System.Windows.Forms.TreeNode[] {
+            treeNode27,
+            treeNode28,
+            treeNode29});
+            System.Windows.Forms.TreeNode treeNode31 = new System.Windows.Forms.TreeNode("Не працює клавіатура");
+            System.Windows.Forms.TreeNode treeNode32 = new System.Windows.Forms.TreeNode("Не працює миша");
+            System.Windows.Forms.TreeNode treeNode33 = new System.Windows.Forms.TreeNode("Не завантажується комп\'ютер");
+            System.Windows.Forms.TreeNode treeNode34 = new System.Windows.Forms.TreeNode("Немає з\'єднання з інтернет мережею");
+            System.Windows.Forms.TreeNode treeNode35 = new System.Windows.Forms.TreeNode("Дзвоніть в поліцію");
+            System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("Пропав комп\'ютер", new System.Windows.Forms.TreeNode[] {
+            treeNode35});
+            System.Windows.Forms.TreeNode treeNode37 = new System.Windows.Forms.TreeNode("У вас комп\'ютер", new System.Windows.Forms.TreeNode[] {
+            treeNode30,
+            treeNode31,
+            treeNode32,
+            treeNode33,
+            treeNode34,
+            treeNode36});
+            System.Windows.Forms.TreeNode treeNode38 = new System.Windows.Forms.TreeNode("У вас телефон");
+            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("У вас шизофренія");
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
@@ -60,44 +60,46 @@
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Location = new System.Drawing.Point(9, 10);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Узел13";
-            treeNode1.Text = "Перевірте підключення всі проводів до монітора і розеток";
-            treeNode2.Name = "Узел14";
-            treeNode2.Text = "Назжміть кнопку ВКЛ на моніторі";
-            treeNode3.Name = "Узел15";
-            treeNode3.Text = "Віднесіть монітор на ремонт";
-            treeNode4.Name = "Узел6";
-            treeNode4.Text = "Не працює монітор";
-            treeNode5.Name = "Узел7";
-            treeNode5.Text = "Не працює клавіатура";
-            treeNode6.Name = "Узел8";
-            treeNode6.Text = "Не працює миша";
-            treeNode7.Name = "Узел9";
-            treeNode7.Text = "Не завантажується комп\'ютер";
-            treeNode8.Name = "Узел10";
-            treeNode8.Text = "Немає з\'єднання з інтернет мережею";
-            treeNode9.Name = "Узел12";
-            treeNode9.Text = "Дзвоніть в поліцію";
-            treeNode10.Name = "Узел11";
-            treeNode10.Text = "Пропав комп\'ютер";
-            treeNode11.Name = "Узел2";
-            treeNode11.Text = "У вас комп\'ютер";
-            treeNode12.Name = "Узел3";
-            treeNode12.Text = "У вас телефон";
-            treeNode13.Name = "Узел4";
-            treeNode13.Text = "У вас шизофренія";
+            treeNode27.Name = "Узел13";
+            treeNode27.Text = "Перевірте підключення всі проводів до монітора і розеток";
+            treeNode28.Name = "Узел14";
+            treeNode28.Text = "Назжміть кнопку ВКЛ на моніторі";
+            treeNode29.Name = "Узел15";
+            treeNode29.Text = "Віднесіть монітор на ремонт";
+            treeNode30.Name = "Узел6";
+            treeNode30.Text = "Не працює монітор";
+            treeNode31.Name = "Узел7";
+            treeNode31.Text = "Не працює клавіатура";
+            treeNode32.Name = "Узел8";
+            treeNode32.Text = "Не працює миша";
+            treeNode33.Name = "Узел9";
+            treeNode33.Text = "Не завантажується комп\'ютер";
+            treeNode34.Name = "Узел10";
+            treeNode34.Text = "Немає з\'єднання з інтернет мережею";
+            treeNode35.Name = "Узел12";
+            treeNode35.Text = "Дзвоніть в поліцію";
+            treeNode36.Name = "Узел11";
+            treeNode36.Text = "Пропав комп\'ютер";
+            treeNode37.Name = "Узел2";
+            treeNode37.Text = "У вас комп\'ютер";
+            treeNode38.Name = "Узел3";
+            treeNode38.Text = "У вас телефон";
+            treeNode39.Name = "Узел4";
+            treeNode39.Text = "У вас шизофренія";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode11,
-            treeNode12,
-            treeNode13});
-            this.treeView1.Size = new System.Drawing.Size(491, 514);
+            treeNode37,
+            treeNode38,
+            treeNode39});
+            this.treeView1.Size = new System.Drawing.Size(369, 418);
             this.treeView1.TabIndex = 0;
             // 
             // statusStrip1
@@ -105,9 +107,10 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 529);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1067, 25);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -115,14 +118,15 @@
             // 
             this.toolStripStatusLabel1.BackColor = System.Drawing.Color.Transparent;
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(52, 20);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(43, 17);
             this.toolStripStatusLabel1.Text = "Статус";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(733, 233);
+            this.button1.Location = new System.Drawing.Point(556, 228);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -131,9 +135,10 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(721, 27);
+            this.radioButton1.Location = new System.Drawing.Point(415, 16);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 21);
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
             this.radioButton1.TabIndex = 6;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "radioButton1";
@@ -142,9 +147,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(721, 70);
+            this.radioButton2.Location = new System.Drawing.Point(415, 51);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(110, 21);
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
             this.radioButton2.TabIndex = 7;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "radioButton2";
@@ -153,9 +159,10 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(721, 110);
+            this.radioButton3.Location = new System.Drawing.Point(415, 83);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(110, 21);
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
             this.radioButton3.TabIndex = 8;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "radioButton3";
@@ -164,9 +171,10 @@
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(721, 149);
+            this.radioButton4.Location = new System.Drawing.Point(415, 115);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(110, 21);
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
             this.radioButton4.TabIndex = 9;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "radioButton4";
@@ -175,20 +183,34 @@
             // radioButton5
             // 
             this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(721, 191);
+            this.radioButton5.Location = new System.Drawing.Point(415, 149);
+            this.radioButton5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(110, 21);
+            this.radioButton5.Size = new System.Drawing.Size(85, 17);
             this.radioButton5.TabIndex = 10;
             this.radioButton5.TabStop = true;
             this.radioButton5.Text = "radioButton5";
             this.radioButton5.UseVisualStyleBackColor = true;
             // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(415, 183);
+            this.radioButton6.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(85, 17);
+            this.radioButton6.TabIndex = 11;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton6);
             this.Controls.Add(this.radioButton5);
             this.Controls.Add(this.radioButton4);
             this.Controls.Add(this.radioButton3);
@@ -197,7 +219,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.treeView1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "ExSystem";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -219,6 +240,7 @@
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
     }
 }
 
--- ExSystem/ExSystem/Form1.cs
+++ ExSystem/ExSystem/Form1.cs
@@ -19,11 +19,11 @@ namespace ExSystem
         }
 
         List<RadioButton> labels = new List<RadioButton>();
-        TreeNodeCollection node;
+        TreeView node;
         private void Form1_Load(object sender, EventArgs e)
         {
-            labels.AddRange(new RadioButton[] { radioButton1, radioButton2, radioButton3, radioButton4, radioButton5 });
-            node = treeView1.Nodes;
+            labels.AddRange(new RadioButton[] { radioButton1, radioButton2, radioButton3, radioButton4, radioButton5, radioButton6 });
+            node = treeView1;
 
             foreach (RadioButton label in labels)
             {
@@ -32,38 +32,38 @@ namespace ExSystem
             labels[0].Select();
             treeView1.CheckBoxes = true;
             int i = 0;
-            foreach (TreeNode o in node)
+            clear();
+            foreach (TreeNode o in node.Nodes)
             {
                 labels[i].Text = o.Text;
                 labels[i].Visible = true;
                 i++;
             }
-            foreach (RadioButton label in labels)
-            {
-                if (label.Text == "")
-                {
-                    label.Visible = false;
-                }
-            }
+            
         }
 
+        TreeNodeCollection currentNodes;
         private void button1_Click(object sender, EventArgs e)
         {
             RadioButton selectedRadio;
+            if (currentNodes == null) {
+                currentNodes = node.Nodes;
+            }
             foreach (RadioButton radio in labels)
             {
                 if (radio.Checked)
                 {
                     selectedRadio = radio;
-                    foreach (TreeNode o in node)
+                    foreach (TreeNode o in currentNodes)
                     {
                         if (o.Text.Equals(selectedRadio.Text))
                         {
                             o.Checked = true;
                             o.Expand();
                             int i = 0;
-                            TreeNode[] nodes = node.Find(o.Text,true);
-                            foreach (TreeNode nd in nodes)
+                            currentNodes = o.Nodes;
+                            clear();
+                            foreach (TreeNode nd in currentNodes)
                             {
                                 labels[i].Text = nd.Text;
                                 labels[i].Visible = true;
@@ -75,6 +75,13 @@ namespace ExSystem
             }
 
 
+        }
+
+        public void clear() {
+            foreach (RadioButton label in labels)
+            {
+                label.Visible = false;
+            }
         }
     }
 }
